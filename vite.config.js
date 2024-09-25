031675a8d4fbcab0d3d2d359493f0ad2b9fbbbd5
Dave Joshua Ariño <156934208+BuffDave@users.noreply.github.com>
2024-09-25 21:17:31 +0800

    commit

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+export default {
+  root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  server: {
+    port: 8080,
+    hot: true
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: 'src',
+    rollupOptions: {
+      input: [
+        'src/index.html',
+        'src/contact.html',
+        'src/privacy.html',
+        'src/projects.html',
+        'src/resume.html',
+        'src/terms.html',
+        'src/docs/Resume.pdf'
+      ], // Specify each HTML file individually
+    },
+  },
+}
